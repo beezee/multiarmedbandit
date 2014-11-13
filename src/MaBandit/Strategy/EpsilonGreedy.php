ef7164ff013cab11af7fc9a2dc874555515e36bd
Brian Zeligson <bzeligson@localytics.com>
2014-11-13 17:04:49 -0500

    Strategy is abstract, move some functionality to it

--- src/MaBandit/Strategy/EpsilonGreedy.php
+++ src/MaBandit/Strategy/EpsilonGreedy.php
@@ -1,10 +1,9 @@
 <?php
 
 namespace MaBandit\Strategy;
-use \prettyArray\PrettyArray;
 
 // TODO - needs tests
-class EpsilonGreedy implements Strategy
+class EpsilonGreedy extends Strategy
 {
   
   private $_exploreEvery;
@@ -22,40 +21,9 @@ class EpsilonGreedy implements Strategy
     return $i;
   }
 
-  public function getWinner($levers)
-  {
-    return (new PrettyArray($levers))
-      ->max_by(function($l) { return $l->getConversionRate(); });
-  }
-
-  // TODO - make selection non deterministic during a tie and test
-  public function getExplorationLevers($levers)
-  {
-    $winner = $this->getWinner($levers);
-    return (new PrettyArray($levers))
-      ->reject(function($key, $l) use($winner) { return $winner == $l; })
-      ->to_a();
-  }
-
-  public function getTotalIterations($levers)
-  {
-    return (new PrettyArray($levers))
-      ->inject(function($k, &$l, &$m) { $m += $l->getDenominator(); });
-  }
-
   public function shouldExplore($levers)
   {
     return (($this->getTotalIterations($levers) + 1) % $this->_exploreEvery) 
       == 0;
-  }
-  
-  public function chooseLever(\MaBandit\Experiment $experiment)
-  {
-    $levers = $experiment->getLevers();
-    if (!$this->shouldExplore($levers))
-      return $this->getWinner($levers);
-    $rest = $this->getExplorationLevers($levers);
-    $k = array_rand($rest);
-    return $rest[$k];
   }
 }
--- src/MaBandit/Strategy/Strategy.php
+++ src/MaBandit/Strategy/Strategy.php
@@ -1,8 +1,46 @@
 <?php
 
 namespace MaBandit\Strategy;
+use \prettyArray\PrettyArray;
 
-interface Strategy
+abstract class Strategy
 {
-  public function chooseLever(\MaBandit\Experiment $experiment);
+
+  abstract public function shouldExplore($levers);
+
+  public function getWinner($levers)
+  {
+    return (new PrettyArray($levers))
+      ->max_by(function($l) { return $l->getConversionRate(); });
+  }
+
+  // TODO - make selection non deterministic during a tie and test
+  public function getExplorationLevers($levers)
+  {
+    $winner = $this->getWinner($levers);
+    return (new PrettyArray($levers))
+      ->reject(function($key, $l) use($winner) { return $winner == $l; })
+      ->to_a();
+  }
+
+  public function getTotalIterations($levers)
+  {
+    return (new PrettyArray($levers))
+      ->inject(function($k, &$l, &$m) { $m += $l->getDenominator(); });
+  }
+  
+  public function chooseExploratoryLever($levers)
+  {
+    $exploratoryLevers = $this->getExplorationLevers($levers);
+    $k = array_rand($exploratoryLevers);
+    return $exploratoryLevers[$k];
+  }
+
+  public function chooseLever(\MaBandit\Experiment $experiment)
+  {
+    $levers = $experiment->getLevers();
+    return ($this->shouldExplore($levers))
+      ? $this->chooseExploratoryLever($levers)
+      : $this->getWinner($levers);
+  }
 }
