b6f07613a4f91c8a1d53242e8a868ae9d220acde
Brian Zeligson <bzeligson@localytics.com>
2014-11-13 01:55:05 -0500

    new exceptions, add underscore src

---
+++ src/MaBandit/Exception/ExperimentNotFoundException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace MaBandit\Exception;
+
+class ExperimentNotFoundException extends \Exception {}
---
+++ src/MaBandit/Exception/InvalidPercentExplorationException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace MaBandit\Exception;
+
+class InvalidPercentExplorationException extends \Exception {};
